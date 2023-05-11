27e17f0e3ee93f4cee08d25e1f5313baa2135f8c
gisa chris <gisachris24@gmail.com>
2023-05-11 17:41:36 -0200

    adding functionality to application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import refreshImage from './refresh.png';
-import arrow from './arrow-key.svg';
-import dots from './dots.svg';
-import trash from './trash.svg';
-
-// selecting the container
-const container = document.querySelector('.container');
+import {container,taskHolder,taskSection,tasklist,addToDom,arrowHolder,button} from './modules/addRemove.js';
 
 // selecting the header section
 const headerSection = document.querySelector('.header');
@@ -17,74 +12,6 @@ refresh.classList.add('refreshButton');
 refresh.src = refreshImage;
 
 headerSection.append(refresh);
-
-// selecting the input container
-const inputSection = document.querySelector('.inputSection');
-
-// selecting the input field
-const inputField = document.getElementById('inputField');
-
-const arrowHolder = document.createElement('img');
-arrowHolder.classList.add('arrow');
-arrowHolder.src = arrow;
-
-inputSection.append(inputField, arrowHolder);
-
-// selecting the button
-const button = document.querySelector('.clearAll');
-
-// implementing list functionality
-
-const taskHolder = [
-  { description: 'wash utensils', completed: false, index: 1 },
-  { description: 'Go to the market', completed: true, index: 2 },
-  { description: 'go for a jog', completed: false, index: 3 },
-];
-
-class Task {
-  constructor(description) {
-    this.index = taskHolder.length;
-    this.description = description;
-    this.completed = false;
-  }
-}
-
-const taskCreator = () => {
-  const newTask = new Task(inputField.value);
-  taskHolder.push(newTask);
-  inputField.value = '';
-};
-
-// create the dom elements to support the array
-const taskSection = document.createElement('section');
-taskSection.classList.add('taskSection');
-container.append(taskSection);
-container.insertBefore(taskSection, button);
-const tasklist = document.createElement('ul');
-tasklist.classList.add('tasklist');
-taskSection.append(tasklist);
-
-const addToDom = () => {
-  // Clear the list
-  tasklist.innerHTML = '';
-
-  taskHolder.forEach((instance) => {
-    const { index, description, completed } = instance;
-
-    const taskListItem = document.createElement('li');
-    taskListItem.classList.add('listInstance');
-    taskListItem.innerHTML = `
-      <input type="checkbox" name="${index}" id="${index}" class='checkbox'>
-      <span class='descriptionText'>${description}</span>
-      <img src=${dots} class='threedots-${index}'>
-      <img src=${trash} class='trash'>
-      `;
-    tasklist.append(taskListItem);
-  });
-};
-
-// render list-section dynamically when page loads
-window.addEventListener('load', addToDom());
 
 const refreshButton = document.querySelector('.refreshButton');
 refreshButton.addEventListener('click', () => {
---
+++ src/modules/addRemove.js
@@ -0,0 +1,198 @@
+import arrow from '../arrow-key.svg';
+import dots from '../dots.svg';
+import trash from '../trash.svg';
+
+// selecting the container
+const container = document.querySelector('.container');
+
+// selecting the input container
+const inputSection = document.querySelector('.inputSection');
+
+// selecting the input field
+export const inputField = document.getElementById('inputField');
+
+//local storage
+const inputStorage = JSON.parse(localStorage.getItem('inputdata')) || {};
+inputField.value = inputStorage.inputField || '';
+
+const inputSave = () => {
+  inputStorage.inputField = inputField.value;
+
+  localStorage.setItem('inputdata', JSON.stringify(inputStorage));
+};
+
+const clearInput = () => {
+  inputStorage.inputField = '';
+
+  localStorage.setItem('inputdata', JSON.stringify(inputStorage));
+};
+
+// attach an event listener to the input field to listen for changes
+inputField.addEventListener('input', inputSave);
+
+//selecting the arrow for entry
+const arrowHolder = document.createElement('img');
+arrowHolder.classList.add('arrow');
+arrowHolder.src = arrow;
+
+inputSection.append(inputField, arrowHolder);
+
+// implementing list functionality
+//const taskHolder = [];
+
+//local storage for array
+const bookSave = () => {
+  localStorage.setItem('taskList', JSON.stringify(taskHolder));
+};
+
+const taskHolder = JSON.parse(localStorage.getItem('taskList')) || [];
+
+class Task {
+  constructor(description) {
+    this.index = taskHolder.length;
+    this.description = description;
+    this.completed = false;
+  }
+}
+
+const taskCreator = () => {
+  const newTask = new Task(inputField.value);
+  taskHolder.push(newTask);
+  inputField.value = '';
+};
+
+// selecting the button
+const button = document.querySelector('.clearAll');
+
+// create the dom elements to support the array
+const taskSection = document.createElement('section');
+taskSection.classList.add('taskSection');
+container.append(taskSection);
+container.insertBefore(taskSection, button);
+const tasklist = document.createElement('ul');
+tasklist.classList.add('tasklist');
+taskSection.append(tasklist);
+
+const addToDom = () => {
+  // Clear the list
+  tasklist.innerHTML = '';
+
+  taskHolder.forEach((instance) => {
+    const { index, description, completed } = instance;
+
+    const taskListItem = document.createElement('li');
+    taskListItem.classList.add('listInstance');
+    taskListItem.setAttribute('data-index',index);
+    taskListItem.innerHTML = `
+      <input type="checkbox" name="${index}" id="${index}" class='checkbox'>
+      <span class='descriptionText' data-index='${index}'>${description}</span>
+      <img src=${dots} class='threedots'data-index='${index}'>
+      <img src=${trash} class='trash' data-index='${index}'>
+      `;
+    tasklist.append(taskListItem);
+
+    const listStruc = document.querySelectorAll('.listInstance');
+    listStruc.forEach((inst) => {
+      // edit description
+      const descriptionTexts = inst.querySelectorAll('.descriptionText[data-index]');
+      descriptionTexts.forEach((span) => {
+        span.addEventListener('click', () => {
+          const input = document.createElement('input');
+          input.classList.add('input-2');
+          input.type = 'text';
+          input.value = span.innerText;
+          
+          input.addEventListener('blur', () => {
+            span.innerText = input.value;
+            instance.description = input.value;
+            setTimeout(() => {
+              input.replaceWith(span);
+            }, 0);
+            localStorage.setItem('taskList', JSON.stringify(taskHolder));
+          });
+          
+          input.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+              span.innerText = input.value;
+              instance.description = input.value;
+              setTimeout(() => {
+                input.replaceWith(span);
+              }, 0);
+            }
+            localStorage.setItem('taskList', JSON.stringify(taskHolder));
+          });          
+          span.replaceWith(input);
+          input.focus();
+        });
+        localStorage.setItem('taskList', JSON.stringify(taskHolder));
+      });
+    });
+    bookSave();
+    });
+    console.log(taskHolder);
+};
+
+inputField.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter' && !(inputField.value === '')) {
+    taskCreator();
+    addToDom();
+    clearInput();    
+  }
+});
+
+arrowHolder.addEventListener('click', () => {
+  if (!(inputField.value === '')) {
+    taskCreator();
+    addToDom();
+    clearInput();
+  }
+});
+
+// render list-section dynamically when page loads
+window.addEventListener('load', () => {
+  addToDom();
+});
+
+//handle clicks
+const handleDeleteClick = (event) => {
+  const listItem = event.target.closest('li');
+  const listItemIndex = parseInt(listItem.dataset.index);
+  const indexInTaskHolder = taskHolder.findIndex(task => task.index === listItemIndex);
+  if (indexInTaskHolder !== -1) {
+    taskHolder.splice(indexInTaskHolder, 1);
+  }else if(indexInTaskHolder === 0) {
+    taskHolder.splice(0, taskHolder.length);
+  }
+  listItem.remove();
+  localStorage.setItem('taskList', JSON.stringify(taskHolder));
+};
+
+
+const handleDotClick = (event) => {
+  const dot = event.target;
+  const listItem = dot.closest('li');
+  const deleteButton = listItem.querySelector('.trash');
+  deleteButton.style.display = 'block';
+  dot.style.display = 'none';
+}
+
+const listRefresher = () => {
+  taskHolder.forEach((item, index) => {
+    item.index = index;
+  });
+}
+
+document.addEventListener('click', (event) => {
+  const isDot = event.target.classList.contains('threedots');
+  const isTrash = event.target.classList.contains('trash');
+  if (isDot) {
+    handleDotClick(event);
+  } else if (isTrash) {
+    handleDeleteClick(event);
+    listRefresher();
+  }
+});
+console.log(taskHolder);
+
+export {container,taskHolder,taskSection,tasklist,addToDom,arrowHolder,button};
+export default taskHolder;
